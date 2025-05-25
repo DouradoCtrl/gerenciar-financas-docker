0e0187ffe02de0b62d1b477438e0acce4fb4a136
DouradoCtrl <gdouradoestudante@gmail.com>
2025-05-25 15:05:44 -0300

    Add news endpoints

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TransacaoController;
 
 
-Route::get('financeiro/transacao/listar', [TransacaoController::class, 'index']);
+Route::get('financeiro/transacao/listar/todos', [TransacaoController::class, 'index']);
 
-Route::post('financeiro/transacao/adicionar', [TransacaoController::class, 'store']);
+Route::post('financeiro/transacao/cadastrar', [TransacaoController::class, 'store']);
+
+Route::get('financeiro/transacao/listar/{id}', [TransacaoController::class, 'show']);
+
+Route::put('financeiro/transacao/editar/{id}', [TransacaoController::class, 'update']);
+
+Route::delete('financeiro/transacao/excluir/{id}', [TransacaoController::class, 'destroy']);
