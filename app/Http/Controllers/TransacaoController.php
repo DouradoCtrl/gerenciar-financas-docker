23a1f919f435756f7bedb7a3fad2b08fd0d470e3
DouradoCtrl <gdouradoestudante@gmail.com>
2025-05-25 15:04:30 -0300

    method show, update, destroy developed

--- app/Http/Controllers/TransacaoController.php
+++ app/Http/Controllers/TransacaoController.php
@@ -9,6 +9,7 @@ class TransacaoController extends Controller
 {
     public function index() {
         $transacao = Transacao::all();
+
         return response()->json([
             'message' => 'Transações listadas com sucesso',
             'transacao' => $transacao       
@@ -23,6 +24,34 @@ class TransacaoController extends Controller
             'message' => 'Transação criada com sucesso',
             'transacao' => $transacao
         ], 201);
+    }
+
+    public function show($id) {
+        $transacao = Transacao::find($id);
+
+        return response()->json([
+            'message' => 'Transação encontrada com sucesso',
+            'transacao:' => $transacao
+        ]);
+    }
+
+    public function update(Request $request, $id) {
+        $transacao = Transacao::find($id);
+        $transacao->update($request->all());
+
+        return response()->json([
+            'message' => 'Transação atualizada com sucesso',
+            'transacao' => $transacao
+        ], 200);
+    }
+
+    public function destroy($id) {
+        $transacao = Transacao::find($id);
+        $transacao->delete();
+
+        return response()->json([
+            'message' => 'Transação excluída com sucesso',
+        ], 200);
     }
 
 }
